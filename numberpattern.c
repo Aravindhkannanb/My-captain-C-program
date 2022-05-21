d5f10d5bdde8df7b8726b163b454efd27a7613e3
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-05-21 23:09:48 +0530

    Add files via upload

---
+++ numberpattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+void main()
+{
+ int i,j,n;
+
+ printf("Enter the value of n:");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+  for(j=1;j<i;j++)
+  {
+         printf(("01"));
+         
+   
+  }
+ printf(" ");
+ }
+
+}

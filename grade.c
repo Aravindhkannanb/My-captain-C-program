de5d89c1d75e10433c1af7d9455a1d8e459fa4ce
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-05-21 21:44:49 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main(){
+    int marks;
+    printf("Enter Marks : ");
+    scanf("%d",&marks);
+   if (marks==100 &&  marks >=85 ){
+       printf("Grade A");
+   }
+   else if (marks <=84 && marks >=70){
+       printf("Grade B");
+   }
+   else if (marks <=69 && marks >= 55){
+       printf("Grade C");
+   }
+   else if (marks <= 54 && marks >=40){
+       printf("Grade D");
+   }
+   else{
+       printf("Grade F");
+   }
+    return 0;
+}
